f42ec7e4b82bda1492cd29dbb4ff6a6758d8a05d
Tan Wenxing <imnotwenxing@gmail.com>
2026-01-06 18:32:27 +0800

    Added login and register pages with customised first name, last name, phone number.
    removed Customer entity as it is replaced by the User entity provided by blazor identity

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<HealthIsWealthUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<HealthIsWealthContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();

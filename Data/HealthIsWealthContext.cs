c5bde48c1c473d68b2a77f431cd633c3faecbb99
Tan Wenxing <imnotwenxing@gmail.com>
2026-01-06 10:14:45 +0800

    Added identity

--- Data/HealthIsWealthContext.cs
+++ Data/HealthIsWealthContext.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HealthIsWealth.Data;
 using HealthIsWealth.Domain;
 
 namespace HealthIsWealth.Data
 {
-    public class HealthIsWealthContext : DbContext
+    public class HealthIsWealthContext(DbContextOptions<HealthIsWealthContext> options) : IdentityDbContext<HealthIsWealthUser>(options)
     {
-        public HealthIsWealthContext (DbContextOptions<HealthIsWealthContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<HealthIsWealth.Domain.Customer> Customer { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Booking> Booking { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Facility> Facility { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.FacilitySport> FacilitySport { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Review> Review { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Sport> Sport { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Timeslot> Timeslot { get; set; } = default!;
-        public DbSet<HealthIsWealth.Domain.Venue> Venue { get; set; } = default!;
+        public DbSet<Customer> Customer { get; set; } = default!;
+        public DbSet<Booking> Booking { get; set; } = default!;
+        public DbSet<Facility> Facility { get; set; } = default!;
+        public DbSet<FacilitySport> FacilitySport { get; set; } = default!;
+        public DbSet<Review> Review { get; set; } = default!;
+        public DbSet<Sport> Sport { get; set; } = default!;
+        public DbSet<Timeslot> Timeslot { get; set; } = default!;
+        public DbSet<Venue> Venue { get; set; } = default!;
     }
 }
